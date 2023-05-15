c9ad9b0a9aac919b93050a0fa6d06683ac750154
Nghia <minhnghia260803@gmail.com>
2023-05-15 11:26:54 +0700

    cc

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,13 +2,7 @@
 'use strict';
 
 const api_key= "bef65ba1f876aa17d8053796d815d7b9";
-
-/*
-        * Fetch data from server
-       * @param {string} URL API url
-       * @param {Function} callback callback
-
-*/ 
+ 
 
 export const fetchData = function (URL, callback) {
     fetch(`${URL}&appid=${api_key}`)
@@ -18,28 +12,15 @@ export const fetchData = function (URL, callback) {
 
 export const url= {
     currentWeather(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units
-        =metric`
-    },
-
-    forecast(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units
-        =metric`
-    },
-
-    airPollution(lat, lon) {
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
-    },
-
-    reverseGeo(lat, lon) {
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
-    },
-
-    /*
-        * @param {string} query Search query e.g.: "London", "New York"
-    */ 
-
-    geo(query) {
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`    
-    },
+        return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`
+      },
+      forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric`
+      },
+      airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}`
+      },
+      reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5`
+      },
 }
